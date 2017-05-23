f90c3c14ff1f4b02a8f35182b34e3bb47af22302
Narayanan Perumbaillam <narayanan.perumbaillam@snowmen.se>
2017-05-23 16:06:13 +0200

    todo

---
+++ one.elm
@@ -0,0 +1,18 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+main =
+  view
+
+
+  --Model
+
+  --view
+
+view =
+  div[]
+   [ input [ type_ "text" ] []
+   , button [] [ text "todo"]
+   ]
+  --update
